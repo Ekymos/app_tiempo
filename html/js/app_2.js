cee1ef3a7501cbe66cf58f65ba443fe263501116
Juan Carlos Hernandez Cardona <juancarlos.hernandez@unir.net>
2016-11-17 11:31:24 +0100

    Limitamos la llamada a la api de la ciudad cuando sea mayor a 2 caracteres

--- html/js/app_2.js
+++ html/js/app_2.js
@@ -1,41 +1,42 @@
- angular
+angular
     .module('apiApp', [])
     .controller('apiAppCtrl', controladorPrincipal);
 
 function controladorPrincipal($scope, $http){
-    var vm=this;
-    vm.buscaCiudad = function(){
-       var url = 'http://api.wunderground.com/api/9d60410990f6709c/conditions/q/'; 
-       if(vm.nombre){
-           url +=  vm.nombre+'.json';
-        }
-        $http.get(url).success(function(respuesta){
-            //console.log("res:", respuesta.response.results[0]);
-            var data = respuesta.response.results;
-            vm.ciudades = data;
-        });
+    var vm = this;
+    vm.buscaCiudad = function(e){
+      var num_string = vm.nombre.length;
+      //console.log(num_string);
+      if(vm.nombre && num_string > 2){
+        var url = 'http://api.wunderground.com/api/9d60410990f6709c/conditions/q/';
+        url +=  vm.nombre+'.json';
+      }
+      $http.get(url).success(function(respuesta){
+        //console.log("res:", respuesta.response.results[0]);
+        var data = respuesta.response.results;
+        vm.ciudades = data;
+      });
     }
-    
+
     vm.valor_ciudad = function(item){
-       var valor = '';
-       vm.nombre = item.name+' '+item.country_name;
+      var valor = '';
+      vm.nombre = item.name+' '+item.country_name;
     }
-   
-    
+
     vm.buscaTiempo = function(item){
        var valor = '';
        var tmp = item.split(' ');
-       var country = tmp[1];  
-       var name = tmp[0]; 
+       var country = tmp[1];
+       var name = tmp[0];
        //vm.valor = item.name+' '+item.country_name;
-       
+
        var url = 'http://api.wunderground.com/api/9d60410990f6709c/forecast/lang:SP/q/';
        var url2 = 'http://api.wunderground.com/api/9d60410990f6709c/conditions/lang:SP/q/';
        if(item){
          url +=  country+'/'+name+'.json';
          url2 +=  country+'/'+name+'.json';
        }
-       
+
        $http.get(url).success(function(respuesta){
          var data2 = respuesta.forecast.simpleforecast.forecastday;
          vm.tiempos_2 = data2[0];
@@ -46,5 +47,5 @@ function controladorPrincipal($scope, $http){
          vm.location = data1.display_location;
        });
     }
-    
+
 }
